834911cc5797f40cd2a36865524f3437dd50580f
Márton Polesznyák <marton.polesznyak@stud.uni-corvinus.hu>
2021-10-01 10:53:01 +0200

    Create "Add" method

--- UserMaintenance/irf2 gyak kozz10/Form1.cs
+++ UserMaintenance/irf2 gyak kozz10/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using irf2_gyak_kozz10.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,6 +13,8 @@ namespace irf2_gyak_kozz10
 {
     public partial class Form1 : Form
     {
+
+        BindingList<User> users = new BindingList<User>();
         public Form1()
         {
             InitializeComponent();
@@ -21,6 +24,20 @@ namespace irf2_gyak_kozz10
             label2.Text = Resource1.FirstName;
             button1.Text = Resource1.Add;
 
+            listBox1.DataSource = users;
+            listBox1.ValueMember = "ID";
+            listBox1.DisplayMember = "FullName";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var u = new User()
+            {
+                LastName = textBox1.Text,
+                FirstName = textBox2.Text
+            };
+
+            users.Add(u);
         }
     }
 }
