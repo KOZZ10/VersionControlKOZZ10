505ae2d9ce16a435b6004fbec62aa7946d6f1057
Polesznyak Marton <marton.polesznyak@stud.uni-corvinus.hu>
2021-10-08 16:44:26 +0200

    Add excel name space

--- ExcelExport/ExcelExport/Form1.cs
+++ ExcelExport/ExcelExport/Form1.cs
@@ -7,18 +7,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
 
 namespace ExcelExport
 {
     public partial class Form1 : Form
     {
+
         RealEstateEntities context = new RealEstateEntities();
         List<Flat> lakasok;
         public Form1()
         {
             InitializeComponent();
             LoadData();
-            dataGridView1.DataSource = lakasok;
+            dataGridView1.DataSource = lakasok;       
         }
 
 
