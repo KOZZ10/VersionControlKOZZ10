52235b31de1050848d6fe575401ef6b2147df3fc
KOZZ10 <marton.polesznyak@stud.uni-corvinus.hu>
2021-12-03 16:09:33 +0100

    Write in output with changing data

--- MicroSimExample/MicroSimExample/Form1.cs
+++ MicroSimExample/MicroSimExample/Form1.cs
@@ -14,6 +14,8 @@ namespace MicroSimExample
 {
     public partial class Form1 : Form
     {
+        Random rng = new Random(1234);
+
         List<Person> Population =null;
         List<BirthProbability> BirthProbabilities = null;
         List<DeathProbability> DeathProbabilities = null;
@@ -23,6 +25,66 @@ namespace MicroSimExample
             Population = GetPopulation(@"C:\temp\nép-teszt.csv");
             BirthProbabilities = GetBirthProbabilities(@"C:\temp\születés.csv");
             DeathProbabilities = GetDeathProbabilities(@"C:\temp\halál.csv");
+
+            for (int year = 2005; year <= 2024; year++)
+            {
+                for (int i = 0; i < Population.Count; i++)
+                { 
+                    SimStep(year, Population[i]);
+                    int NbrOfMales = (from x in Population
+                                     where x.Gender == Gender.Male && x.IsAlive
+                                     select x).Count();
+
+                    int NbrOfFemales = (from x in Population
+                                      where x.Gender == Gender.Female && x.IsAlive
+                                      select x).Count();
+
+                    Console.WriteLine(string.Format(
+
+                        "Év: {0}\nFiuk: {1}\nLányok: {2}\n",
+                        year,
+                        NbrOfMales,
+                        NbrOfFemales
+                      ));
+                    
+                }
+            }
+        }
+
+        private void SimStep(int year, Person person)
+        {
+            //Ha halott akkor kihagyjuk, ugrunk a ciklus következő lépésére
+            if (!person.IsAlive) return;
+
+            // Letároljuk az életkort, hogy ne kelljen mindenhol újraszámolni
+            byte age = (byte)(year - person.BirthYear);
+
+            // Halál kezelése
+            // Halálozási valószínűség kikeresése
+            double pDeath = (from x in DeathProbabilities
+                             where x.Gender == person.Gender && x.Age == age
+                             select x.P).FirstOrDefault();
+            // Meghal a személy?
+            if (rng.NextDouble() <= pDeath)
+                person.IsAlive = false;
+
+            //Születés kezelése - csak az élő nők szülnek
+            if (person.IsAlive && person.Gender == Gender.Female)
+            {
+                //Szülési valószínűség kikeresése
+                double pBirth = (from x in BirthProbabilities
+                                 where x.Age == age
+                                 select x.P).FirstOrDefault();
+                //Születik gyermek?
+                if (rng.NextDouble() <= pBirth)
+                {
+                    Person újszülött = new Person();
+                    újszülött.BirthYear = year;
+                    újszülött.NbrOfChildren = 0;
+                    újszülött.Gender = (Gender)(rng.Next(1, 3));
+                    Population.Add(újszülött);
+                }
+            }
         }
 
         public List<Person> GetPopulation(string csvPath)
@@ -56,7 +118,7 @@ namespace MicroSimExample
                     var line = sr.ReadLine().Split(';');
                     birthProbabilities.Add(new BirthProbability()
                     {
-                        Age = int.Parse(line[0]),                      
+                        Age = byte.Parse(line[0]),                      
                         NbrOfChildren = int.Parse(line[1]),
                         P = double.Parse(line[2].Replace(",","."))
                     });
@@ -76,7 +138,7 @@ namespace MicroSimExample
                     var line = sr.ReadLine().Split(';');
                     deathProbabilities.Add(new DeathProbability()
                     {
-                        Age = int.Parse(line[0]),
+                        Age = byte.Parse(line[0]),
                         Gender = (Gender)Enum.Parse(typeof(Gender), line[1]),
                         P = double.Parse(line[2].Replace(",", "."))
                     });
