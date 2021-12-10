8458fad27e4202c13ddcdf9f554ce75dae2878e2
KOZZ10 <marton.polesznyak@stud.uni-corvinus.hu>
2021-12-10 11:29:07 +0100

    game completed, error resolved by teacher

---
+++ WorldsHardestGame/WorldsHardestGame/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace WorldsHardestGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblGenereation = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblGenereation
+            // 
+            this.lblGenereation.AutoSize = true;
+            this.lblGenereation.Location = new System.Drawing.Point(235, 11);
+            this.lblGenereation.Name = "lblGenereation";
+            this.lblGenereation.Size = new System.Drawing.Size(66, 13);
+            this.lblGenereation.TabIndex = 0;
+            this.lblGenereation.Text = "1. generáció";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(238, 27);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Visible = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(835, 472);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblGenereation);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblGenereation;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
